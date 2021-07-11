fbf1ddcb5cfc0fefe4f6200900409f1370d9f433
vinothkumar <net.vinothkumar@gmail.com>
2021-07-11 23:28:00 +0200

    crud spring app

---
+++ src/main/java/com/example/demo/SubscriberManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberManagementApplication.class, args);
+	}
+
+}
